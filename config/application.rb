8a3b8710f44515b0e50fc055edfb6d9826c91a9a
Anton Kaznacheev <azbrut@gmail.com>
2016-10-11 14:04:33 +0300

    Some for devise views & pass recover send

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require 'carrierwave/orm/activerecord'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsDevisePundit
+module Tortonbook
   class Application < Rails::Application
 
     config.generators do |g|
@@ -32,7 +32,7 @@ module RailsDevisePundit
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+     config.i18n.default_locale = :ru
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
