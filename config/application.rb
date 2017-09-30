69a782c9a995cab028de9f1c37e0251756836eb1
Anton Kaznacheev <azbrut@gmail.com>
2017-09-30 12:48:54 +0300

    1

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Tortonbook
                        request_specs: false
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
     end
+    config.middleware.use Rack::Deflater
     config.autoload_paths += %W[#{config.root}/lib #{config.root}/app/uploaders]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
