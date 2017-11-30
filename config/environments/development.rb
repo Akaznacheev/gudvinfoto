a2418955ce6fe95a74cb1c6eee9646a4d07c4c5b
Anton Kaznacheev <azbrut@gmail.com>
2017-12-01 02:42:30 +0300

    logger setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.logger = Logger.new(Rails.root.join('log', Rails.env + '.log'), 5, 100 * 1024 * 1024)
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
+  config.logger = Logger.new(Rails.root.join('log', Rails.env + '.log'), 5, 100 * 1024 * 1024)
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
